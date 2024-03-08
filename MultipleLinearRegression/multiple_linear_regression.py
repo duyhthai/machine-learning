fbdd1ffda9a1e349aca72eba54f27c267a5de60e
Duy Thai <duyhthai@gmail.com>
2024-03-08 11:39:08 +0700

    Multiple Linear Regression

--- MultipleLinearRegression/multiple_linear_regression.py
+++ MultipleLinearRegression/multiple_linear_regression.py
@@ -2,8 +2,11 @@
 
 # Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
@@ -11,20 +14,29 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Encoding categorical data
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-print(X_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Training the Multiple Linear Regression model on the Training set
-from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-
+regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
+y_predict = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+
+# reshape to vertically
+print(np.concatenate(
+    (y_predict.reshape(len(y_predict), 1), y_test.reshape(len(y_test), 1)),
+    1))
+
+# Making a single prediction (for example the profit of a startup with
+# R&D Spend = 160000, Administration Spend = 130000, Marketing Spend = 300000 and State = 'California')
+print(regressor.predict([[1, 0, 0, 160000, 130000, 300000]]))
+
+# Getting the final linear regression equation with the values of the coefficients
+print(regressor.coef_)
+print(regressor.intercept_)
